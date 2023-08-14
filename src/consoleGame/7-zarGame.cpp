31e7a4eeb35783cca294b9d3629623564a24cb66
Ilkay Beydah Saglam <beydahsaglam@hotmail.com>
2023-08-14 23:36:25 +0300

    Update 7-zarGame.cpp

--- src/consoleGame/7-zarGame.cpp
+++ src/consoleGame/7-zarGame.cpp
@@ -1,140 +1,126 @@
 #include <iostream>
+#include <cstdlib>
+#include <ctime>
 using namespace std;
 
-/*ZAR ATMA OYUNU*/
-/*Kullanıcının hedefi, 
-kolu çevirdiğinde 1 ile 5 arasındaki üç zarın hepsinin 5 gelmesini sağlamaktır. 
-Başlangıçta kullanıcının hesabında 1.000.000 dolar bulunmaktadır 
-ve her kolu çevirdiğinde 500 dolar kaybetmektedir. 
-Ancak, eğer kullanıcıya kolu çevirdiğinde üç zar da 5 gelirse, 
-1000 dolar kazanma fırsatı elde eder. 
-Bu oyun, şansa dayalı bir deneyimi mükemmel bir gerilimle birleştirerek, 
-büyük ödülü yakalamak için heyecan dolu bir fırsat sunmaktadır*/
-
-void output(int screen)
-{
-	if(screen == 0)
-	{
-		cout <<
-		"=====================================\n"
-		"=  W  I  N  -  W  I  N  -  W  I  N  =\n"
-		"============== G A M E ==============\n"
-		"=====================================\n"
-		"= Author: Ilkay Beydah Saglam       =\n"
-		"= Version: 0.0.1                    =\n"
-		"= Copyright: Open Source            =\n"
-		"= Description: a Small Concole Game =\n"
-		"=====================================\n"
-		"\nPress any key and enter to start: ";	
-	}
-	
-	else if(screen == 1)
-	{
-		cout << 
-		"1- Start ($500)\n"
-		"2- Exit\n"
-		"\nEnter Your Choice: ";
-	}
-	
-	else if(screen == 3)
-	{
-		cout << 
-		"\n"
-		"=====================\n"
-		"= G A M E - O V E R =\n"
-		"=====================\n"
-		"\n";
-	}
+void output(int screen) {
+    if (screen == 0) {
+        cout <<
+        "=====================================\n"
+        "=  W  I  N  -  W  I  N  -  W  I  N  =\n"
+        "============== G A M E ==============\n"
+        "=====================================\n"
+        "= Author: Ilkay Beydah Saglam       =\n"
+        "= Version: 0.0.1                    =\n"
+        "= Copyright: Open Source            =\n"
+        "= Description: a Small Concole Game =\n"
+        "=====================================\n"
+        "\nPress any key and enter to start: ";
+    } else if (screen == 1) {
+        cout << 
+        "1- Start ($500)\n"
+        "2- Exit\n"
+        "\nEnter Your Choice: ";
+    } else if (screen == 2) {
+        cout << 
+        "\n"
+        "=====================\n"
+        "= G A M E - O V E R =\n"
+        "=====================\n"
+        "\n";
+    }
 }
 
-void wrongOutput(int choiceLen)
-{
-	cout << "\nWrong Choice\n";
-	
-	if(choiceLen < 2)
-	{
-		cout << "Remaining chances: " << 3 - choiceLen << '\n';
-	}
-	
-	else if(choiceLen < 3)
-	{
-		cout << "ITS LATS CHANCE\n";
-	}
-	
-	else
-	{
-		cout << "You Are Lost Your Chance\n";
-	}
+void wrongOutput(int choiceLen) {
+    cout << "\nWrong Choice\n";
+    
+    if (choiceLen < 2) {
+        cout << "Remaining chances: " << 3 - choiceLen << '\n';
+    } else if (choiceLen < 3) {
+        cout << "ITS LAST CHANCE\n";
+    } else {
+        cout << "You Have Lost Your Chance\n";
+    }
 }
 
-void screenClear()
-{
-	cout << "\x1B[2J\x1B[H";	
+void screenClear() {
+    cout << "\x1B[2J\x1B[H";    
 }
 
-int choiceFinder(int choice)
-{
-	if (choice < 1 || choice > 2)
-	{
-		return 404;
-	}
-	
-	else
-	{
-		return choice;
-	}
+int choiceFinder(int choice) {
+    if (choice < 1 || choice > 2) {
+        return 404;
+    } else {
+        return choice;
+    }
 }
 
-/*INT FONKSIYON 4 = RANDOM ATAMASI*/
+int randomZar() {
+    return (rand() % 5) + 1;
+}
 
-int main()
-{
-	/*RANDOM VARIABLE*/
-	/*random slot1, slot2, slot3*/
-	
-	/*MAIN VARIABLE*/
-	int choice, choiceLen = 0;
-	string anyKey;
-	
-	/*GAME VARİABLE*/
-	int dolar = 1000000;
-	int buy = 500;
-	
-	output(0);
-	cin >> anyKey;
-	
-	screenClear();
-	
-	do
-	{
-		//MAIN SCREEN
-		cout << "\nYour Money: $" << dolar << "\n\n";
-		output(1);
-		
-		cin >> choice;
-		choice = choiceFinder(choice);
-		
-		screenClear();
-		
-		if(choice == 404)
-		{
-			wrongOutput(choiceLen);
-			if(choiceLen > 2)
-			{
-				choice = 2;
-			}
-			choiceLen++;
-		}
-		
-		//GAME SCREEN
-		else if(choice == 1)
-		{
-			choiceLen = 0;
-			dolar -= buy;
-		}
-	}
-	while(choice != 2);
-	output(3);
-	
-	return 0;
+int main() {
+    /*RANDOM VARIABLE*/
+    int slot1, slot2, slot3;
+
+    /*MAIN VARIABLE*/
+    int choice, choiceLen = 0;
+    string anyKey;
+
+    /*GAME VARIABLE*/
+    int dolar = 10000;
+    int buy = 500;
+
+    output(0);
+    cin >> anyKey;
+
+    screenClear();
+
+    do {
+        //MAIN SCREEN
+        cout << "\nYour Money: $" << dolar << "\n\n";
+        output(1);
+
+        cin >> choice;
+        choice = choiceFinder(choice);
+
+        screenClear();
+
+        if (choice == 404) {
+            wrongOutput(choiceLen);
+            if (choiceLen > 2) {
+                choice = 2;
+            }
+            choiceLen++;
+        }
+        
+        //GAME SCREEN
+        else if (choice == 1) {
+            if (dolar < 500) {
+                cout << "You Don't Have Enough Money.\n";
+            } else {
+                choiceLen = 0;
+
+                /*Random Ataması*/
+                srand(time(0));
+
+                slot1 = randomZar();
+                slot2 = randomZar();
+                slot3 = randomZar();
+
+                cout << "Slots: " << slot1 << " - " << slot2 << " - "  << slot3 << '\n';
+
+                if (slot1 == 5 && slot2 == 5 && slot3 == 5) {
+                    cout << "WIN! WIN! WIN!\n";
+                    dolar += 1000;
+                } else {
+                    cout << "You Lose.\n";
+                    dolar -= buy;
+                }
+            }
+        }
+    } while (choice != 2);
+    output(2);
+
+    return 0;
 }
