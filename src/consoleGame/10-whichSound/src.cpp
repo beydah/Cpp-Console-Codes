79ceba25684b65d650e87494483f43ccab5fa3e0
Ilkay Beydah Saglam <beydahsaglam@hotmail.com>
2023-08-17 22:50:10 +0300

    Add files via upload

---
+++ src/consoleGame/10-whichSound/src.cpp
@@ -0,0 +1,73 @@
+#include <iostream>
+using namespace std;
+
+/*--------------------------------------------------*/
+/*GLOBAL VARIABLES*/
+
+/*GLOBAL FILE VARIABLE*/
+
+/*GLOBAL MAIN VARIABLE*/
+
+/*GLOBAL MAIN CHOICE VARIABLE*/
+
+/*GLOBAL GAME VARIABLE*/
+
+/*GLOBAL GAME CHOICE VARIABLE*/
+
+/*--------------------------------------------------*/
+/*FONKSIYONLAR*/
+
+/*FONKSIYON 1 OUTPUT*/
+
+/*FONKSIYON 2 WRONG OUTPUT*/
+
+/*FONKSIYON 3 CHOICE FINDER*/
+
+/*FONKSIYON 4 RANDOM CREATOR*/
+
+/*FONKSIYON 5 SCREEN CLEAR*/
+
+/*--------------------------------------------------*/
+
+int main()
+{
+	/*--------------------------------------------------*/
+	/*PRIVATE VARIABLES*/
+	
+	/*PRIVATE FILE VARIABLE*/
+	
+	/*PRIVATE MAIN VARIABLE*/
+	
+	/*PRIVATE MAIN CHOICE VARIABLE*/
+	
+	/*PRIVATE GAME VARIABLE*/
+	
+	/*PRIVATE GAME CHOICE VARIABLE*/
+	
+	/*--------------------------------------------------*/
+	
+	do
+	{
+		/*--------------------------------------------------*/
+		/*MAIN SCREEN*/
+		
+		do
+		{
+			
+		}
+		while(0);
+		
+		/*--------------------------------------------------*/
+		/*GAME SCREEN*/
+		
+		while(0)
+		{
+			
+		}
+		
+		/*--------------------------------------------------*/
+	}
+	while(0);
+	
+	return 0;
+}
